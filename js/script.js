26f35ff613b2bfbe3fad3a6d6f9cae6dd2e72fe6
Ahmad Ajmi <ahmadajmi@outlook.com>
2014-04-12 11:29:00 +0200

    Added .editorconfig and some code changes

--- js/script.js
+++ js/script.js
@@ -4,64 +4,64 @@
  * Inspired by http://pxtoem.com/
  */
 
-function log(string) {
-	return window.console.log(string);
-}
+ function log(string) {
+   return window.console.log(string);
+ }
 
-var $pxInput = $('#px'),
-	$emInput = $('#em');
+ var $pxInput = $('#px'),
+ $emInput = $('#em');
 
-function convertToEm(target, context) {
-	context = context | 16;
-	return target / context;
-}
+ function convertToEm(target, context) {
+   context = context | 16;
+   return target / context;
+ }
 
-function convertToPx(target, context){
-	context = context | 16;
-	return target * context;
-}
+ function convertToPx(target, context){
+   context = context | 16;
+   return target * context;
+ }
 
-function clearInputs(){
-	$pxInput.val('');
-	$emInput.val('');
-}
+ function clearInputs(){
+   $pxInput.val('');
+   $emInput.val('');
+ }
 
-$pxInput.on('focus', function(){
-	clearInputs();
-})
+ $pxInput.on('focus', function(){
+   clearInputs();
+ })
 
-$emInput.on('focus', function(){
-	clearInputs();
-})
+ $emInput.on('focus', function(){
+   clearInputs();
+ })
 
-$('input[type="submit"]').on("click",function(event){
+ $('input[type="submit"]').on("click",function(event){
 
-	event.preventDefault();
+   event.preventDefault();
 
-	var pxValue = document.getElementById("px").value.replace(/[^0-9.]/g, ''),
-		emValue = document.getElementById("em").value.replace(/[^0-9.]/g, ''),
-		resultId = document.getElementById("result");
+   var pxValue = document.getElementById("px").value.replace(/[^0-9.]/g, ''),
+   emValue = document.getElementById("em").value.replace(/[^0-9.]/g, ''),
+   resultId = document.getElementById("result");
 
-	if (!pxValue && !emValue) {
-		log("Please Insert PX or EM values");
-		return;
-	}
+   if (!pxValue && !emValue) {
+    log("Please Insert PX or EM values");
+    return;
+  }
 
-	if (pxValue) {
-		var pxToEmResult =  convertToEm(pxValue) + "em";
-	}
+  if (pxValue) {
+    var pxToEmResult =  convertToEm(pxValue) + "em";
+  }
 
-	if (emValue) {
-		var emToPxResult =  convertToPx(emValue) + "px";
-	}
+  if (emValue) {
+    var emToPxResult =  convertToPx(emValue) + "px";
+  }
 
-	resultId.innerHTML = (pxValue ? pxValue + "/" + 16 : emValue + "*" + 16)  + " = " + (pxValue ? pxToEmResult : emToPxResult );
+  resultId.innerHTML = (pxValue ? pxValue + "/" + 16 : emValue + "*" + 16)  + " = " + (pxValue ? pxToEmResult : emToPxResult );
 
 });
 
-resultId = document.getElementById("result");
+ resultId = document.getElementById("result");
 
-$("#change").keyup(function(){
-	console.log(12);
-	resultId.innerHTML = $(this).val();
-})
+ $("#change").keyup(function(){
+   console.log(12);
+   resultId.innerHTML = $(this).val();
+ })
